ff2cd627379584878df5d259d93bd20109794594
zafkielkurumi <563693600@qq.com>
2021-07-22 11:57:36 +0800

    update webworker

--- src/app/page/gvg/gvg/gvg.component.ts
+++ src/app/page/gvg/gvg/gvg.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit, ViewChildren } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import {
   PcrApiService,
-  ChangelogServiceApi,
   NoticeApiService,
 } from '@app/apis';
 import { storageNames } from '@app/constants';
 import {
   RediveDataService,
   RediveService,
-  ServerUnChara,
   StorageService,
 } from '@app/core';
 import {
@@ -23,20 +20,21 @@ import {
   ServerType,
   Task,
 } from '@app/models';
-import { Subject } from 'rxjs';
-import { catchError, finalize, takeUntil, timeout } from 'rxjs/operators';
-import { cloneDeep, reject } from 'lodash';
+import {  finalize, } from 'rxjs/operators';
+import { cloneDeep } from 'lodash';
 import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
-import { AddUnHaveComponent } from '../widgets/add-un-have/add-un-have.component';
 import { AddTaskComponent } from '../widgets/add-task/add-task.component';
 import { filterTask } from '../services/filterTask';
+
 import { FilterResultService } from '../services/filter-result.service';
 import { NoticeComponent } from '../widgets/notice/notice.component';
 import { NzCollapsePanelComponent } from 'ng-zorro-antd/collapse';
 import { environment } from '@src/environments/environment';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
-// import { createWorker } from '@src/app/uitl/createWorker';
-// import { str } from '../services/test';
+import {getLocalWorkerUrl} from '@app/util/createWorker';
+// import {workerString} from './fitler-worker-str';
+
+
 
 @Component({
   selector: 'pcr-gvg',
@@ -58,13 +56,16 @@ export class GvgComponent implements OnInit {
     private filterResultSrv: FilterResultService,
     private nzNotificationSrv: NzNotificationService
   ) { }
-
-  charaList: Chara[] = []; // 角色列表
-  stage = 1; // 阶段
-  stageOption = []; // 阶段option
-  gvgTaskList: GvgTask[] = []; // 初始作业列表
-  filterGvgTaskList: GvgTask[] = []; // 根据筛选条件显示的列表
-  OnDestroySub = new Subject();
+    // 角色列表
+  charaList: Chara[] = []; 
+   // 阶段
+  stage = 1;
+   // 阶段option
+  stageOption = [];
+   // 初始作业列表,缓存所有结果
+  gvgTaskList: GvgTask[] = [];
+   // 根据筛选条件显示的列表
+  filterGvgTaskList: GvgTask[] = [];
   autoSetting: CanAutoType[] = [
     CanAutoType.auto,
     CanAutoType.harfAuto,
@@ -84,9 +85,10 @@ export class GvgComponent implements OnInit {
       value: CanAutoType.auto,
     },
   ];
-  loading = false; // 删除loading
-  filterLoading = false; // 筛选loading
-  searchLoading = false; // 搜索loading
+  // 筛选loading
+  filterLoading = false;
+  // 搜索loading 
+  searchLoading = false; 
   clanBattleList = []; // 会战期次
   clanBattleId = null; // 当前会战期次
   serverType = ServerType.jp;
@@ -111,8 +113,8 @@ export class GvgComponent implements OnInit {
   showLink = environment.showLink;
   updateCnTaskLoading = false;
   bossNumberList = [1, 2, 3, 4, 5];
-  serverUnCharas: ServerUnChara;
   taskType = 'all';
+  blobUrl = '';
 
   ngOnInit(): void {
     this.dealServerType();
@@ -120,19 +122,8 @@ export class GvgComponent implements OnInit {
     this.dealServerOperate();
     this.usedList = this.storageSrv.localGet(storageNames.usedList) ?? [];
     this.removedList = this.storageSrv.localGet(storageNames.removedList) ?? [];
-    this.rediveDataSrv
-      .getUnHaveCharaOb()
-      .pipe(takeUntil(this.OnDestroySub))
-      .subscribe((res) => {
-        this.serverUnCharas = res;
-      });
-
     this.toggleServer();
-  }
 
-  // 未拥有角色
-  get unHaveCharaList(): Chara[] {
-    return this.serverUnCharas?.[this.serverType] ?? [];
   }
 
   dealServerOperate() {
@@ -205,6 +196,9 @@ export class GvgComponent implements OnInit {
         this.clanBattleId = this.clanBattleList[0].clanBattleId;
         this.battleIdChange(this.clanBattleId);
       });
+      getLocalWorkerUrl('https://cdn.jsdelivr.net/gh/pcrgvg/statics@1626924994/worker/232.db2c145a667d1c22ee72.js').then(url => {
+        this.blobUrl = url;
+      })
   }
 
   getNotice() {
@@ -255,36 +249,6 @@ export class GvgComponent implements OnInit {
     this.rediveSrv.changeImgSource(url);
   }
 
-  toggleUsed(event, task: Task) {
-    const index = this.usedList.findIndex((r) => r === task.id);
-    if (index > -1) {
-      this.usedList.splice(index, 1);
-    } else {
-      this.usedList.push(task.id);
-    }
-    this.storageSrv.localSet(storageNames.usedList, this.usedList);
-  }
-  toggleRemoved(event, task: Task) {
-    const index = this.removedList.findIndex((r) => r === task.id);
-    if (index > -1) {
-      this.removedList.splice(index, 1);
-    } else {
-      this.removedList.push(task.id);
-    }
-    this.storageSrv.localSet(storageNames.removedList, this.removedList);
-  }
-
-  autoColor(canAuto: number) {
-    switch (canAuto) {
-      case CanAutoType.auto:
-        return '#68B9FF';
-      case CanAutoType.harfAuto:
-        return '#1cbbb4';
-      case CanAutoType.manual:
-      default:
-        return '#FF2277';
-    }
-  }
 
   trackByBossFn(_: number, boss: GvgTask): number {
     return boss.id;
@@ -292,67 +256,6 @@ export class GvgComponent implements OnInit {
 
   trackByTaskFn(_: number, task: Task): number {
     return task.id;
-  }
-
-  trackByCharaFn(_: number, chara: Chara): number {
-    return chara.prefabId;
-  }
-
-  clickStop() { }
-
-  delteConfirm(boss: GvgTask, task: Task) {
-    // this.deleteRef.triggerOk()
-    this.loading = true;
-    this.pcrApi
-      .deleteTask(task.id, this.serverType)
-      .pipe(finalize(() => (this.loading = false)))
-      .subscribe((res) => {
-        const index = boss.tasks.findIndex((r) => r.id === task.id);
-        boss.tasks.splice(index, 1);
-      });
-  }
-
-  delteCancel() {
-    this.deleteRef.destroy();
-  }
-
-  addUnHave() {
-    this.modalSrc.create({
-      nzContent: AddUnHaveComponent,
-      nzComponentParams: {
-        server: this.serverType,
-      },
-      nzFooter: null,
-      nzWidth: '80%',
-      nzMaskClosable: false,
-    });
-  }
-
-  addTask(task?: Task, bossId?: number) {
-    const bossList = this.gvgTaskList.map((r) => ({
-      id: r.id,
-      prefabId: r.prefabId,
-      unitName: r.unitName,
-    }));
-    const modalRef = this.modalSrc.create({
-      nzContent: AddTaskComponent,
-      nzComponentParams: {
-        bossId,
-        task,
-        bossList,
-        stageOption: this.stageOption,
-      },
-      nzFooter: null,
-      nzWidth: '80%',
-      nzMaskClosable: false,
-    });
-    modalRef.afterClose.subscribe((r) => {
-      const contentRef = modalRef.getContentComponent();
-      if (contentRef.gvgTaskList.length) {
-        this.dealGvgTaskList(contentRef.gvgTaskList);
-        this.stage = contentRef.validateForm.getRawValue().stage;
-      }
-    });
   }
 
   openNotice() {
@@ -372,7 +275,7 @@ export class GvgComponent implements OnInit {
   /**
    * 筛刀
    */
-  filter() {
+  async filter() {
     const filterGvgTaskList = cloneDeep(this.filterGvgTaskList);
     const [bossList, taskList] = [[], []];
     filterGvgTaskList.forEach((r) => {
@@ -390,76 +293,69 @@ export class GvgComponent implements OnInit {
       throw new Error('选择至少一个boss');
     }
     this.filterLoading = true;
-
-    setTimeout(() => {
-      const filterResult = filterTask({
-        bossList: taskList,
-        removedList: this.storageSrv.localGet(storageNames.removedList) ?? [],
-        usedList: this.storageSrv.localGet(storageNames.usedList) ?? [],
-        unHaveCharas: this.unHaveCharaList,
-        server: this.serverType,
-      });
-
-      this.filterLoading = false;
-      this.filterResultSrv.setFilterResult(filterResult);
-      this.filterResultSrv.setBosslist(bossList);
-      this.router.navigate(['/gvg/result']);
-    }, 500);
-
+    const unHaveCharas = this.rediveDataSrv.unHaveCharas[this.serverType];
 
 
     /** 
-    * 由于同源策略,无法加载加载cdn中的worker, 并且火狐不支持importScripts
-    * EDGE 不支持使用blob
+    * 由于同源策略,无法加载cdn中的worker, 并且火狐不支持importScripts
+    * 法1：全部使用blob字符串，但是极其难以维护
+    * 法2：先打包生成worker文件，再修改路径进行打包,稍微好维护一点
     */
-    // try {
+    try {
+      // 方法1
+      // const blob = new Blob([workerString, `onmessage = function({data}) {
+      //   const res = filterTask(data);
+      //   postMessage(res);
+      // }`], {type: 'text/javascript'});
+      // const worker = new Worker(URL.createObjectURL(blob))
 
-    //   // let worker: Worker;
-    //   // if( environment.production) {
-    //   //   worker =  createWorker('https://cdn.jsdelivr.net/gh/pcrgvg/statics@1626531153/0.2606a39a918b8678c74d.worker.js');
-    //   // } else {
-    //   //   worker = new Worker('../work/filter.worker', { type: 'module' });
-    //   // }
-    //   const blob = new Blob([str,`onmessage = function({data}) { console.log(data); const res = filterTask(data); postMessage(res); }`]);
-    //   const worker = new Worker(window.URL.createObjectURL(blob));
-    //   // const worker = new Worker('https://aikurumi.cn/0.7ad4428b1f8695d2e696.worker.js');
-    //   worker.onmessage = ({ data }) => {
-    //     console.log(`worker message: ${data.length}`);
-    //     this.filterLoading = false;
-    //     // this.storageSrv.sessionSet(Constants.filterResult, data.slice(0, 200));
-    //     // window.open('/gvgresult', '');
-    //     this.filterResultSrv.setFilterResult(data);
-    //     this.filterResultSrv.setBosslist(bossList);
-    //     this.router.navigate(['/gvg/result']);
-    //     worker.terminate();
-    //   };
+      // 方法2
+      // const worker = new Worker(new URL('../work/filter.worker' , import.meta.url), {type: 'module'});
+      const worker = new Worker(this.blobUrl)
+      worker.onmessage = ({ data }) => {
+        console.log(`worker message: ${data.length}`);
+        this.filterLoading = false;
+        this.filterResultSrv.setFilterResult(data);
+        this.filterResultSrv.setBosslist(bossList);
+        this.router.navigate(['/gvg/result']);
+        worker.terminate();
+      };
 
-    //   worker.postMessage({
-    //     bossList: taskList,
-    //     removedList: this.storageSrv.localGet(storageNames.removedList) ?? [],
-    //     usedList: this.storageSrv.localGet(storageNames.usedList) ?? [],
-    //     unHaveCharas: this.unHaveCharaList,
-    //     server: this.serverType,
-    //   });
-    //   worker.onerror = (err) => {
-    //     this.filterLoading = false;
-    //   };
-    // } catch (error) {
-    //   setTimeout(() => {
-    //     const filterResult = filterTask({
-    //       bossList: taskList,
-    //       removedList: this.storageSrv.localGet(storageNames.removedList) ?? [],
-    //       usedList: this.storageSrv.localGet(storageNames.usedList) ?? [],
-    //       unHaveCharas: this.unHaveCharaList,
-    //       server: this.serverType,
-    //     });
+      worker.postMessage({
+        bossList: taskList,
+        removedList: this.storageSrv.localGet(storageNames.removedList) ?? [],
+        usedList: this.storageSrv.localGet(storageNames.usedList) ?? [],
+        unHaveCharas: unHaveCharas,
+        server: this.serverType,
+      });
+      worker.onerror = (err) => {
+        console.log(err);
+        this.filterLoading = false;
+      };
 
-    //     this.filterLoading = false;
-    //     this.filterResultSrv.setFilterResult(filterResult);
-    //     this.filterResultSrv.setBosslist(bossList);
-    //     this.router.navigate(['/gvg/result']);
-    //   }, 500);
-    // }
+      // getLocalWorkerUrl('https://cdn.jsdelivr.net/gh/pcrgvg/statics@1626531153/0.2606a39a918b8678c74d.worker.js').then(url => {
+      //   let worker: Worker = new Worker(url);
+       
+      // })
+      // worker = new Worker('https://cdn.jsdelivr.net/gh/pcrgvg/statics@1626531153/0.2606a39a918b8678c74d.worker.js');
+   
+    } catch (error) {
+      console.log(error)
+      setTimeout(() => {
+        const filterResult = filterTask({
+          bossList: taskList,
+          removedList: this.storageSrv.localGet(storageNames.removedList) ?? [],
+          usedList: this.storageSrv.localGet(storageNames.usedList) ?? [],
+          unHaveCharas: unHaveCharas,
+          server: this.serverType,
+        });
+
+        this.filterLoading = false;
+        this.filterResultSrv.setFilterResult(filterResult);
+        this.filterResultSrv.setBosslist(bossList);
+        this.router.navigate(['/gvg/result']);
+      }, 500);
+    }
 
   }
 
@@ -547,4 +443,37 @@ export class GvgComponent implements OnInit {
     return task.autoDamage ? task.autoDamage : task.damage;
   }
 
+  // 删除作业
+   onTaskDelete(task: Task, boss: GvgTask) {
+    const index = boss.tasks.findIndex((r) => r.id === task.id);
+    boss.tasks.splice(index, 1);
+   }
+
+  // 添加作业
+  addTask(task?: Task, bossId?: number) {
+    const bossList = this.gvgTaskList.map((r) => ({
+      id: r.id,
+      prefabId: r.prefabId,
+      unitName: r.unitName,
+    }));
+    const modalRef = this.modalSrc.create({
+      nzContent: AddTaskComponent,
+      nzComponentParams: {
+        bossId,
+        task,
+        bossList,
+        stageOption: this.stageOption,
+      },
+      nzFooter: null,
+      nzWidth: '80%',
+      nzMaskClosable: false,
+    });
+    modalRef.afterClose.subscribe((r) => {
+      const contentRef = modalRef.getContentComponent();
+      if (contentRef.gvgTaskList.length) {
+        this.dealGvgTaskList(contentRef.gvgTaskList);
+        this.stage = contentRef.validateForm.getRawValue().stage;
+      }
+    });
+  }
 }
--- src/app/page/gvg/work/filter.worker.ts
+++ src/app/page/gvg/work/filter.worker.ts
@@ -1,7 +1,314 @@
 /// <reference lib="webworker" />
-import { filterTask } from '../services/filterTask';
+
+import { Task, GvgTask, Chara, ServerType } from '../../../models';
+
+type BossTask = Task & {
+  bossId: number;
+  prefabId: number;
+  borrowChara?: Chara;
+  index: number;
+};
+interface FilterTaskParams {
+  bossList: GvgTask[];
+  usedList: number[];
+  removedList: number[];
+  unHaveCharas: Chara[];
+  server: ServerType;
+}
+
+export function cloneDeep(params: any) {
+    return JSON.parse(JSON.stringify(params))
+}
+
+export function flatTask(bossList: GvgTask[], removedList: number[]): BossTask[] {
+  const list = cloneDeep(bossList);
+  const tasks: BossTask[] = [];
+  // flat task
+  list.forEach((boss) => {
+    boss.tasks.forEach((task) => {
+      // 1为尾刀不计入
+      if (!haveRemoved(task, removedList) && task.type != 1) {
+        tasks.push({
+          bossId: boss.id,
+          prefabId: boss.prefabId,
+          index: boss.index,
+          ...task,
+        });
+      }
+    });
+  });
+  return tasks;
+}
+
+export function haveRemoved(task: Task, removedList: number[]): boolean {
+  if (removedList.includes(task.id)) {
+    return true;
+  }
+  return false;
+}
+
+/**
+ *
+ * @param bossList
+ * 返回所有长度为k的组合 BossTask[]
+ * Array<Array<BossTask>>
+ */
+ export function combine(bossTask: BossTask[], k: number): BossTask[][] {
+  const result: BossTask[][] = [];
+  const subResult: BossTask[] = [];
+
+  const combineSub = (start: number, subResult: BossTask[]) => {
+    /// subResult长度符合k，放入result
+    if (subResult.length === k) {
+      result.push(subResult.slice(0));
+      return;
+    }
+    const len = subResult.length;
+    /// 还需要多少个元素才能符合条件，若bossTask剩余的长度不够，则不符合继续循环
+    const conditionLen = bossTask.length - (k - len) + 1;
+    for (let i = start; i < conditionLen; i++) {
+      subResult.push(bossTask[i]);
+      combineSub(i + 1, subResult);
+      subResult.pop();
+    }
+  };
+  combineSub(0, subResult);
+  return result;
+}
+
+/**
+ *
+ * @param prefabIds  重复的角色
+ * @param bossTasks
+ * 重复几次代表要借几次, 借完符合
+ */
+ export function repeatCondition(
+  repeateCharas: number[],
+  unHaveCharas: number[],
+  bossTasks: BossTask[]
+): [boolean, BossTask[]] {
+  const map = new Map<number, number>();
+  for (const prefabId of [...repeateCharas, ...unHaveCharas]) {
+    const charaCount = map.get(prefabId) ?? 0;
+    map.set(prefabId, charaCount + 1);
+  }
+  /// 解决 bossTask.borrowChara引用问题
+  const bossTasksTemp = cloneDeep(bossTasks);
+
+  for (const bossTask of bossTasksTemp) {
+     // 若包含未拥有角色，该作业必定要借该角色
+    let unHaveChara: Chara = null;
+    for (const k of unHaveCharas) {
+      unHaveChara = bossTask.charas.find((chara) => chara.prefabId === k);
+      if (unHaveChara) {
+        break;
+      }
+    }
+
+    if (unHaveChara) {
+      const charaCount = map.get(unHaveChara.prefabId);
+      if (charaCount > 0 && !bossTask.borrowChara) {
+        bossTask.borrowChara = unHaveChara;
+        map.set(unHaveChara.prefabId, charaCount - 1);
+        continue;
+      }
+    }
+    // UNHAVE END
+    let repeatChara: Chara = null;
+    //
+    for (const k of repeateCharas) {
+      repeatChara = bossTask.charas.find((chara) => chara.prefabId === k);
+      if (repeatChara) {
+        const charaCount = map.get(k);
+        if (charaCount > 0 && !bossTask.borrowChara) {
+          bossTask.borrowChara = repeatChara;
+          map.set(k, charaCount - 1);
+          break;
+        }
+      }
+    }
+  }
+
+  const values = [...map.values()];
+  return [values.every((v) => v === 0), bossTasksTemp];
+}
+
+/**
+ * 处理未拥有角色, 当前组合所使用的角色是否包含未拥有角色
+ */
+ export const filterUnHaveCharas = (
+  charas: Chara[],
+  unHaveCharas: Chara[]
+): number[] => {
+  const unHaveCharaPrefabIds: number[] = [];
+  for (const chara of unHaveCharas) {
+    if (charas.findIndex((c) => c.prefabId === chara.prefabId) > -1) {
+      unHaveCharaPrefabIds.push(chara.prefabId);
+    }
+  }
+
+  return unHaveCharaPrefabIds;
+};
+
+/**
+ * 处理一组作业中已使用的作业数，无返回0
+ */
+ export const countUsed = (t: BossTask[], usedList: number[]): number => {
+  return t.reduce((prev, current) => {
+    if (usedList.includes(current.id)) {
+      return prev + 1;
+    }
+    return prev;
+  }, 0);
+};
+
+/**
+ *  处理已使用最多的排在前面
+ * 筛选结果按照分数从高到低排序，已使用的在前
+ */
+ export function fliterResult(
+  bossTasks: BossTask[][],
+  unHaveCharas: Chara[],
+  usedList: number[],
+  server: ServerType
+): BossTask[][] {
+  const tempArr: BossTask[][][] = [[], [], [], []]; /// 依次为包含0/1/2/3个已使用作业组
+
+  for (const bossTask of bossTasks) {
+    const set = new Set(); // 查重
+    const repeateChara: number[] = []; // 重复的角色
+    const charas = []; // 当前组合使用的角色
+    /// 查重
+    // let total = 0;
+    // const startTime = new Date().getTime();
+    for (const task of bossTask) {
+      for (const chara of task.charas) {
+        const size = set.size;
+        charas.push(chara);
+        set.add(chara.prefabId);
+        /// 如果长度不变，说明是重复的
+        if (size === set.size) {
+          repeateChara.push(chara.prefabId);
+        }
+      }
+    }
+    /// 未拥有的算作重复
+    const unHaves = filterUnHaveCharas(charas, unHaveCharas);
+    const [b, t] = repeatCondition(repeateChara, unHaves, bossTask);
+    if (b) {
+      const usedCount = countUsed(t, usedList);
+      tempArr[usedCount].push(t);
+    }
+  }
+  const r = tempArr.map((r) => sortByScore(r, server));
+  r.reverse();
+  // const res = r.flat();
+  const res = r.reduce((arr, val) => arr.concat(val), []);
+  return res;
+}
+
+/**
+ *
+ * @param arr 按照分数排序，暂时分数系数为4阶段
+ */
+ export function sortByScore(arr: BossTask[][], server: ServerType) {
+  const tempArr: BossTask[][] = cloneDeep(arr);
+
+  const scoreFactor = {
+    1: {
+      1: 1.2,
+      2: 1.2,
+      3: 1.3,
+      4: 1.4,
+      5: 1.5,
+    },
+    2: {
+      1: 1.6,
+      2: 1.6,
+      3: 1.8,
+      4: 1.9,
+      5: 2,
+    },
+    3: {
+      1: 2,
+      2: 2,
+      3: 2.4,
+      4: 2.4,
+      5: 2.6,
+    },
+    4: {
+      1: 3.5,
+      2: 3.5,
+      3: 3.7,
+      4: 3.8,
+      5: 4.0,
+    },
+    5: {
+      1: 3.5,
+      2: 3.5,
+      3: 3.7,
+      4: 3.8,
+      5: 4.0,
+    },
+    6: {
+      1: 3.5,
+      2: 3.5,
+      3: 3.7,
+      4: 3.8,
+      5: 4.0,
+    },
+  };
+
+  tempArr.sort((a, b) => {
+    let [aScore, bScore] = [0, 0];
+    a.forEach((task) => {
+      aScore += task.damage * scoreFactor[task.stage][task.index];
+    });
+    b.forEach((task) => {
+      bScore += task.damage * scoreFactor[task.stage][task.index];
+    });
+    return bScore - aScore;
+  });
+  return tempArr;
+}
+
+export const filterTask = ({
+  bossList,
+  usedList,
+  removedList,
+  unHaveCharas,
+  server,
+}: FilterTaskParams): BossTask[][] => {
+  if (!bossList.length) {
+    return [];
+  }
+
+  const bossTask: BossTask[] = flatTask(bossList, removedList);
+  let bossTasks: BossTask[][] = combine(bossTask, 3);
+
+  let result: BossTask[][] = fliterResult(
+    bossTasks,
+    unHaveCharas,
+    usedList,
+    server
+  );
+
+  if (!result.length) {
+    bossTasks = combine(bossTask, 2);
+    result = fliterResult(bossTasks, unHaveCharas, usedList, server);
+  }
+
+  if (!result.length) {
+    bossTasks = combine(bossTask, 1);
+    result = fliterResult(bossTasks, unHaveCharas, usedList, server);
+  }
+
+  return result;
+};
+
 
 addEventListener('message', ({ data }) => {
+  console.log('start')
   const res = filterTask(data);
   postMessage(res);
 });
